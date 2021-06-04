9f4df73adca45a5cb3666c99e84299a9ea3758ba
ATLJoeReed <atljoereed@gmail.com>
2021-06-04 18:28:11 -0400

    More research

--- test_helpers.py
+++ test_helpers.py
@@ -9,14 +9,15 @@ from utils import helpers
 playwright = sync_playwright().start()
 
 
-url = 'https://www.legis.ga.gov'
-intercept_routes = ['api/news/house', 'api/news/senate', 'api/sessions/currentDay'] # noqa
+# url = 'https://www.legis.ga.gov'
+# intercept_routes = ['api/news/house', 'api/news/senate', 'api/sessions/currentDay'] # noqa
 
 url = 'https://www.legis.ga.gov/legislation/all'
-intercept_routes = ['api/sessions', 'api/members/']
+# intercept_routes = ['api/sessions', 'api/members/']
+intercept_routes = ['api/Legislation/']
 
 results = helpers.intercept_api_calls(playwright, url, intercept_routes)
-print(len(results))
+# print(len(results.get('https://www.legis.ga.gov/api/Legislation/Search/20/0').get('results')))
 print(results)
 
 playwright.stop()
