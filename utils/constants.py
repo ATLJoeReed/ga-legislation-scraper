52e939bb77cf4980aaf61283d171253c0036ea1e
ATLJoeReed <atljoereed@gmail.com>
2021-07-13 21:34:30 -0400

    Build legislative summary process

--- utils/constants.py
+++ utils/constants.py
@@ -20,18 +20,12 @@ GA_LEGISLATION_ROUTES = {
         'url': 'https://www.legis.ga.gov/members/senate',
         'intercept_routes': ['/api/members/list/{current_session}?chamber=2'],
     },
-    'all_legislations': {
-        'url': [
-            'https://www.legis.ga.gov/legislation/all',
-            'https://www.legis.ga.gov/search?s={current_session}&p={page}&rc={record_count}', # noqa
-        ],
-        'intercept_routes': ['/api/Legislation/Search/20/{page}'],
+    'legislative_summaries': {
+        'url': 'https://www.legis.ga.gov/legislation/all',
+        'intercept_routes': ['/api/Legislation/Search/20/0'],
     },
     'legislative_details': {
         'url': 'https://www.legis.ga.gov/legislation/{legislation_id}',
-        'intercept_routes': [
-            '/api/legislation/detail',
-            '/api/legislation/html',
-        ],
+        'intercept_routes': ['/api/legislation/detail', '/api/legislation/html'], # noqa
     }
 }
