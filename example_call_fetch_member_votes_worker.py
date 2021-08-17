7d644f76cacbd373c65daa12a7da8cab755e010c
ATLJoeReed <atljoereed@gmail.com>
2021-08-16 20:01:09 -0400

    Run across both house and senate for multiple bills

--- example_call_fetch_member_votes_worker.py
+++ example_call_fetch_member_votes_worker.py
@@ -6,6 +6,8 @@ from workers import fetch_member_votes
 
 votes = [
     (18348, 162, 'House Vote #162'),
+    (18349, 163, 'House Vote #163'),
+    (18604, 251, 'Senate Vote #251'),
 ]
 
 results = fetch_member_votes.process(votes)
