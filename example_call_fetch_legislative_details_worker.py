ce98f3221bc6721aa049c2f46389598d397fad43
ATLJoeReed <atljoereed@gmail.com>
2021-08-19 20:18:06 -0400

    Update example runner

--- example_call_fetch_legislative_details_worker.py
+++ example_call_fetch_legislative_details_worker.py
@@ -5,9 +5,16 @@ from workers import fetch_legislative_details
 
 
 legislative_ids = [
+    '58786',
+    '58787',
+    '58788',
+    '58789',
+    '58790',
+    '58791',
+    '58792',
+    '58793',
     '58911',
     '59252',
-
 ]
 
 results = fetch_legislative_details.process(legislative_ids)
