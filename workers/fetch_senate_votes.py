38ff49ee7479e73809f766324639eae72c5b485a
ATLJoeReed <atljoereed@gmail.com>
2021-10-10 21:23:24 -0400

    Build tests and adjust worker for session parameters

--- workers/fetch_senate_votes.py
+++ workers/fetch_senate_votes.py
@@ -8,9 +8,14 @@ from playwright.async_api import async_playwright
 from utils import constants, helpers
 
 
-async def get_senate_voters(browser, logger):
+async def get_senate_voters(browser, session, logger):
     url, intercept_routes = helpers.get_url_intercept_routes('senate_votes', logger) # noqa
-    params = {'current_session': constants.CURRENT_SESSION}
+    if session:
+        url = f'{url}?session={session}'
+        logger.info(f'URL adjusted to {url}')
+    else:
+        session = constants.CURRENT_SESSION    
+    params = {'session': session}
     adjusted_routes = helpers.update_list_item(intercept_routes, params, logger) # noqa
     logger.info('Intercepting senate vote route')
     context = await browser.new_context()
@@ -18,16 +23,16 @@ async def get_senate_voters(browser, logger):
     return helpers.extract_first_item(results, 'senate_vote', logger)
 
 
-async def main(logger):
+async def main(session, logger):
     async with async_playwright() as playright:
         browser = await playright.chromium.launch()
-        results = await get_senate_voters(browser, logger)
+        results = await get_senate_voters(browser, session, logger)
     return results
 
 
-def process():
+def process(session=None):
     logger = helpers.setup_logger_stdout(os.path.basename(__file__))
     logger.info('<<Starting to fetch senate votes>>')
-    results = asyncio.run(main(logger))
+    results = asyncio.run(main(session, logger))
     logger.info('<<Ending fetching senate votes>>')
     return results
