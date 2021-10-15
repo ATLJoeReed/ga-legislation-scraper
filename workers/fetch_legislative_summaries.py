7933bb57cb62b0bf5974aff8cd05e1fa9498cae8
ATLJoeReed <atljoereed@gmail.com>
2021-10-14 20:51:07 -0400

    Adjust process and build tests

--- workers/fetch_legislative_summaries.py
+++ workers/fetch_legislative_summaries.py
@@ -8,26 +8,30 @@ from playwright.async_api import async_playwright
 from utils import helpers
 
 
-async def get_current_session(browser, logger, derived_url):
+async def get_current_session(browser, derived_url, session, logger):
     url, intercept_routes = helpers.get_url_intercept_routes('legislative_summaries', logger) # noqa
+    if session:
+        url = f'https://www.legis.ga.gov/search?s={session}&p=1'
+        logger.info(f'URL adjusted to {url}') 
     if derived_url:
         url = derived_url
+        logger.info(f'URL adjusted to {url}') 
     logger.info('Intercepting session route')
     context = await browser.new_context()
     results = await helpers.intercept_api_calls(context, url, intercept_routes)
     return helpers.extract_first_item(results, 'summaries', logger)
 
 
-async def main(logger, derived_url):
+async def main(derived_url, session, logger):
     async with async_playwright() as playright:
         browser = await playright.chromium.launch()
-        results = await get_current_session(browser, logger, derived_url)
+        results = await get_current_session(browser, derived_url, session, logger)
     return results
 
 
-def process(derived_url=None):
+def process(derived_url=None, session=None):
     logger = helpers.setup_logger_stdout(os.path.basename(__file__))
     logger.info('<<Starting to fetch legislative summaries>>')
-    results = asyncio.run(main(logger, derived_url))
+    results = asyncio.run(main(derived_url, session, logger))
     logger.info('<<Ending fetching legislative summaries>>')
     return results
